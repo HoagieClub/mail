845075e30c665618b5432b1f4760d9d1dce33e0a
Gagik <gagik@pop-os.localdomain>
2021-07-10 03:36:51 -0400

    Initial

---
+++ next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    env: {
+        AUTH0_SECRET: process.env.AUTH0_SECRET,
+        AUTH0_BASE_URL: process.env.AUTH0_BASE_URL,
+        AUTH0_ISSUER_BASE_URL: process.env.AUTH0_ISSUER_BASE_URL,
+        AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
+        AUTH0_CLIENT_SECRET: process.env.AUTH0_CLIENT_SECRET,
+    },
+}
