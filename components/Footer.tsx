944a3f0b8138f5bce40050be0d96531c82857681
gagik <gagik@princeton.edu>
2021-08-24 16:20:30 -0400

    Remove unnecessary width

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,11 +4,9 @@ import Link from 'next/link'
 
 export default function Footer() {
     return (
-    <Pane display="flex" justifyContent="center" alignItems="center"  width="100%" fontSize="16pt" marginX={majorScale(1)}> 
+    <Pane display="flex" justifyContent="center" height="100px" paddingBottom="30px" alignItems="center" fontSize="14pt" marginX={majorScale(1)}> 
         <Pane maxWidth="600px" >
-        <p>
         Hoagie Mail was built by Princeton students as part of Hoagie Club. <b><u><Link href='https://tally.so/r/mRzdlm'>Have any feedback? Let us know.</Link></u></b>
-        </p>
        </Pane>
     </Pane>
     )
