11481d1f67eb89f15c0de6c5044c16dd363c51f5
Jtech3029 <93494401+Jtech3029@users.noreply.github.com>
2025-10-29 20:13:51 -0400

    DEV-238 Removed Old "code" and "state" Query Parameter Code (#51)
    * Removed Old "code" and "state" Query Parameter Code
    
    removed old code that manually removes the "code" and "state"
    query parameters from the URL since it was used for an old version
    of auth0, but is not needed anymore.
    
    * cleaned up unused code

--- app/digest/page.tsx
+++ app/digest/page.tsx
@@ -1,10 +1,9 @@
 'use client';
 
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 
 import { withPageAuthRequired } from '@auth0/nextjs-auth0/client';
 import { Spinner } from 'evergreen-ui';
-import { useRouter } from 'next/navigation';
 import useSWR, { useSWRConfig } from 'swr';
 
 import ErrorMessage from '@/components/ErrorMessage';
@@ -16,7 +15,6 @@ export default withPageAuthRequired(() => {
     const [errorMessage, setErrorMessage] = useState('');
     const [success, setSuccess] = useState(false);
     const [loading, setLoading] = useState(false);
-    const router = useRouter();
     const fetcher = (url: string) => fetch(url).then((r) => r.json());
     const { data, error } = useSWR('/api/hoagie/stuff/user', fetcher);
 
@@ -65,18 +63,6 @@ export default withPageAuthRequired(() => {
             mutate('/api/hoagie/stuff/user');
         }
     };
-
-    useEffect(() => {
-        const queryParams = new URLSearchParams(location.search);
-
-        if (queryParams.has('code')) {
-            queryParams.delete('code');
-            queryParams.delete('state');
-            // TODO: add support for other params to persist using
-            // queryParam.toString() or remove the queryParams method
-            router.replace('/app');
-        }
-    }, [router]);
 
     // TODO: Handle error properly.
     if (!data) {
