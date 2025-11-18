f6d50222c99f6e0b4d2438e1d209b3ff6030597a
Jenny Fan <40123244+jfmath04@users.noreply.github.com>
2025-11-17 23:42:49 -0500

    DEV-380 Set Up Django Project (#53)
    * Skeleton Django project set up for backend
    
    * Add environment variables
    
    * Combine mail views into one
    
    * Configure Ruff for linting

---
+++ backend/hoagiemail/api/mail_view.py
@@ -0,0 +1,23 @@
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+
+class MailView(APIView):
+	def post(self, request) -> Response:
+		# Logic to send mail
+		return Response({"status": "OK", "message": "Mail sent successfully"}, status=status.HTTP_200_OK)
+
+	def get(self, request) -> Response:
+		# Logic to get scheduled mails
+		return Response(
+			{"status": "OK", "message": "Scheduled mails retrieved successfully"}, status=status.HTTP_200_OK
+		)
+
+	def put(self, request) -> Response:
+		# Logic to update a scheduled mail
+		return Response({"status": "OK", "message": "Scheduled mail updated successfully"}, status=status.HTTP_200_OK)
+
+	def delete(self, request) -> Response:
+		# Logic to delete a scheduled mail
+		return Response({"status": "OK", "message": "Scheduled mail deleted successfully"}, status=status.HTTP_200_OK)
