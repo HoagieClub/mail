80e7aba7c49546500342bb2fb12128d4f923ceeb
Jenny Fan <40123244+jfmath04@users.noreply.github.com>
2026-01-20 19:52:22 -0500

    Update scheduled mail fields

--- backend/hoagiemail/migrations/0001_initial.py
+++ backend/hoagiemail/migrations/0001_initial.py
@@ -57,9 +57,9 @@ class Migration(migrations.Migration):
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('custom_sender_name', models.CharField(blank=True, max_length=30)),
                 ('header_text', models.CharField(max_length=150)),
-                ('body_text', models.CharField(max_length=2000)),
+                ('body_text', models.TextField()),
                 ('scheduled_at', models.DateTimeField(db_index=True)),
-                ('created_at', models.DateTimeField()),
+                ('created_at', models.DateTimeField(auto_now_add=True)),
                 ('sender', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
             ],
             options={
