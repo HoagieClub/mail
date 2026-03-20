0a6d192fc5ecc3cc52f40cae5e12b6cd38c6ac95
Karen Gao <anxin.gao@gmail.com>
2026-03-20 01:30:13 -0400

    made stuffuserview get post handler

--- backend/hoagiemail/api/stuff_user_view.py
+++ backend/hoagiemail/api/stuff_user_view.py
@@ -1,12 +1,34 @@
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from hoagiemail.models import StuffPost
+from hoagiemail.serializers import StuffPostSerializer
+import logging
 
+logger = logging.getLogger(__name__)
 
 class StuffUserView(APIView):
 	def get(self, request) -> Response:
-		# Logic to get user posts
-		return Response({"status": "OK", "message": "User posts retrieved successfully"}, status=status.HTTP_200_OK)
+		# return a user's stuff post, if one exists
+		user = request.user
+		try:
+			post = StuffPost.objects.get(author=user)
+			serializer = StuffPostSerializer(post)
+			return Response(serializer.data, status=status.HTTP_200_OK)
+
+		except StuffPost.DoesNotExist:
+			logger.error(f"No stuff post found for {user.email}")
+			return Response(
+				{"error": "You do not have a stuff post yet."},
+				status=status.HTTP_404_NOT_FOUND,
+			)
+
+		except Exception as e:
+			logger.error(f"Error retrieving stuff post for {user.email}: {e}")
+			return Response(
+				{"error": "Unexpected error retrieving stuff post."},
+				status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+			)
 
 	def post(self, request) -> Response:
 		# Logic to make a post
