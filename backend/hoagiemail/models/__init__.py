ff03a1320dd2959a615da81ddcb9757680a8e0a2
Cryptex-github <64497526+asze17@users.noreply.github.com>
2026-03-24 23:09:39 -0400

    Add UserSerializer to model export

--- backend/hoagiemail/models/__init__.py
+++ backend/hoagiemail/models/__init__.py
@@ -1,6 +1,6 @@
 from .mail import ScheduledEmail
 from .stuff import Category, StuffPost, Tag
-from .user import User
+from .user import User, UserSerializer
 from .user_limits import UserLimits
 
-__all__ = ["ScheduledEmail", "Tag", "Category", "StuffPost", "User", "UserLimits"]
+__all__ = ["ScheduledEmail", "Tag", "Category", "StuffPost", "User", "UserLimits", "UserSerializer"]
