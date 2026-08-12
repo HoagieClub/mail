1fe6de904ed7b1c15f0a9f9eb2a4870e35dd474b
Cryptex-github <64497526+asze17@users.noreply.github.com>
2026-08-12 17:34:09 -0400

    Add clarifying message regarding Huo College

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 
 import { useUser } from '@auth0/nextjs-auth0';
 import {
+    Alert,
     Pane,
     majorScale,
     minorScale,
@@ -22,6 +23,12 @@ export default function Index() {
     else if (user) {
         Profile = (
             <Pane>
+                <Alert intent="none">
+                    On Friday, August 14th, HoagieMail will begin delivering emails to HuoHub, instead of Westwire, 
+                    as part of the University's naming transition for Huo College (formerly New College West). 
+                    No action from any user is needed. HoagieMail will continue to function as intended.
+                     All Huo students are automatically subscribed to the new listserv.<em></em></Alert>
+                <br/>
                 <Link href='/app'>
                     <Button
                         height={56}
